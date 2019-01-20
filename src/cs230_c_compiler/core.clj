e61852ae8a6afe85ec740d4c0dd5ef268e1fc5f2
Isaac Khor <s.github@isaackhor.com>
2019-01-20 00:55:23 -0500

    Pass everything to instaparse

--- src/cs230_c_compiler/core.clj
+++ src/cs230_c_compiler/core.clj
@@ -1,7 +1,16 @@
 (ns cs230-c-compiler.core
+  (:require [instaparse.core :as ip]
+            [clojure.pprint :refer [pprint]])
   (:gen-class))
+
+;(def parser (ip/parser (slurp "src/cs230_c_compiler/grammar.ebnf")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [[grammar-path source-path] args
+        grammar (slurp grammar-path)
+        source (slurp source-path)
+        parser (ip/parser grammar :auto-whitespace :standard)]
+    (println "Parsing the input...")
+    (pprint (parser source))))
