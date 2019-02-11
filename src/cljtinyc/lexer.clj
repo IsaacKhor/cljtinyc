0ed42780309a82151488f81e7b753c312378c652
Isaac Khor <s.github@isaackhor.com>
2019-02-10 22:46:18 -0500

    Write rest of scanning algorithm

--- src/cljtinyc/lexer.clj
+++ src/cljtinyc/lexer.clj
@@ -3,12 +3,9 @@
             [slingshot.slingshot :refer [throw+]]))
   ; (:import (dk.brics.automaton RegExp Automaton RunAutomaton State)))
 
-(defn make-lexer [patterns]
-  "Creates a lazy [string] -> [token] lexer from patterns"
-  (fn [stream]))
-    
-
-(defn get-next-token [patmap linenum linestr prevcharnum]
+(defn- get-next-token [patmap linestr linenum prevcharnum]
+  "Given patterns and lexer state, get next token. Patterns must begin
+   with '^' to match start of line."
   (let [; Trim whitespace since it doesn't matter in C
         trimmed-line (str/triml linestr)
         num-trimmed (- (count linestr) (count trimmed-line))
@@ -37,10 +34,44 @@
                                (inc linenum)
                                linenum)]
             {:token {:type tok-type
-                     :text match-text}
+                     :text match-text
+                     :line linenum
+                     :char cur-char}
              :next-lex-state {:linenum next-linenum
                               :linestr rest-line
                               :charnum next-char}})
           ; No match; try another pattern
           (recur (rest patterns)))))))
+
+(defn- get-next-line [lines]
+  (let [; Trim because trailing whitespace will mess with lexer and it
+        ; doesn't matter in C anyways
+        first-line (first lines)
+        trimmed (if (nil? first-line) "" (str/trimr first-line))]
+    [trimmed (rest lines)]))
+
+(defn- lex
+  ([patterns lines] 
+   (let [[first-line rest-lines] (get-next-line lines)] 
+     (lex patterns [] first-line rest-lines 1 1)))
+  ([patterns tokens cur-line rest-lines linenum charnum]
+   (let [res (get-next-token patterns cur-line linenum charnum)
+         new-token (:token res)
+         new-tokens (conj tokens new-token)
+         new-state (:next-lex-state res)
+         line-exhausted? (empty? (:linestr new-state))
+         [new-cur-line new-rest-lines] (if line-exhausted? 
+                                         (get-next-line rest-lines) 
+                                         [(:linestr new-state) rest-lines])
+         [new-linenum new-charnum] (if line-exhausted?
+                                    [(inc linenum) 1]
+                                    [linenum charnum])]
+         ; This line is empty and there's no next line (at EOF)
+     (if (and line-exhausted? (empty? new-rest-lines))
+       new-tokens
+       (recur patterns new-tokens new-cur-line new-rest-lines new-linenum new-charnum)))))
     
+(defn make-lexer [patterns]
+  "Creates a [string] -> [token] lexer from patterns"
+  (fn lexer [string]
+    (lex patterns (str/split-lines string))))
