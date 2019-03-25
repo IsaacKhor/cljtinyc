90062287083d315ec41f7559f10cfd7159db4744
Isaac Khor <s.github@isaackhor.com>
2019-03-25 01:14:43 -0400

    Implement FOLLOW algorithm

--- src/cljtinyc/llparser.clj
+++ src/cljtinyc/llparser.clj
@@ -6,6 +6,18 @@
 (defn ^:private remove-epsilon [s]
   (s/difference s #{:epsilon}))
 
+(defn ^:private get-all-symbols [grammar]
+  (set (flatten grammar)))
+
+(defn ^:private get-grammar-meta [grammar]
+  (let [all-symbols (get-all-symbols grammar)
+        non-terminals (set (map first grammar))
+        terminals (s/difference all-symbols non-terminals)
+        modified-terminals (into terminals #{:epsilon :end-of-file})]
+    {:all-symbols all-symbols
+     :non-terminals non-terminals
+     :terminals modified-terminals}))
+
 (defn ^:private get-first-for-beta [beta fsets]
   (if (empty? beta)
     ; If there are no more elements in beta, then every element of beta might
@@ -64,9 +76,6 @@
     ; (println "Non-Terminals: " non-terminals)
     (merge nt-fsets t-fsets)))
 
-(defn ^:private get-all-symbols [grammar]
-  (set (flatten grammar)))
-
 (defn calculate-first-sets
   "Calculates the FIRST sets for each non-terminal in the grammar.
    Grammar should be specified as a list of productions of the form 
@@ -78,9 +87,81 @@
    Any symbol on the left hand side of a production is assumed to be a
    non-terminal, and those not on the right are assumed to be a terminal."
   [grammar]
-  (let [all-symbols (get-all-symbols grammar)
-        non-terminals (set (map first grammar))
-        terminals (s/difference all-symbols non-terminals)
-        modified-terminals (into terminals #{:epsilon :end-of-file})
-        fsets (setup-first-sets modified-terminals non-terminals)]
+  (let [{:keys [all-symbols non-terminals terminals]} (get-grammar-meta grammar)
+        fsets (setup-first-sets terminals non-terminals)]
     (get-first-sets grammar fsets)))
+
+(defn ^:private production-follow-contrib 
+  "Calculates the production's contribution to the overall FOLLOW set."
+  [production first-sets follow-sets non-terminals]
+  (let [nt (first production)
+        rhs (rest production)]
+    (loop [; RHS symbols are processed in REVERSE because each symbol needs
+           ; to affect the FOLLOW set of the one before itself
+           beta (reverse rhs)
+           folsets follow-sets
+           ; The trailing suffix of the entire production is the FOLLOW of the
+           ; non-terminal on the left deriving the entire expression, since
+           ; that is what follows the rule
+           trailer (nt follow-sets)]
+      ; (println beta folsets trailer)
+      (if (empty? beta)
+        folsets
+        (let [beta-last (first beta)]
+          (if (contains? non-terminals beta-last)
+            ; If beta-last is a non-terminal, then the FOLLOW of the symbol
+            ; before beta-last is whatever trailing suffices that we have 
+            ; accumulated
+            (let [prev-beta-last-folset (beta-last folsets)
+                  ; Add the trailer to the FOLLOW of beta-last
+                  new-beta-last-folset (s/union trailer prev-beta-last-folset)
+                  first-beta-last (beta-last first-sets)
+                  new-trailer 
+                  ; If beta-last can derive epsilon, then the trailer is 
+                  ; whatever has accumulated PLUS FIRST(beta-last), since
+                  ; in the case that beta-last derives epsilon, the symbol
+                  ; before it will have whatever is trailing follow it
+                  (if (contains? first-beta-last :epsilon)
+                    (s/union trailer (remove-epsilon first-beta-last))               
+                    first-beta-last)]
+              (recur (rest beta)
+                     (assoc folsets beta-last new-beta-last-folset)
+                     new-trailer))
+            ; If beta-last is a terminal symbol, then the trailing suffix for
+            ; anything before beta-last is just be the FIRST of itself, which
+            ; should be the set containing only itself
+            (recur (rest beta) folsets (beta-last first-sets))))))))
+
+(defn ^:private process-prods-for-follow
+  [productions first-sets follow-sets non-terminals]
+  (if (empty? productions)
+    follow-sets
+    (recur (rest productions)
+           first-sets
+           (production-follow-contrib 
+              (first productions) first-sets follow-sets non-terminals)
+           non-terminals)))
+
+(defn ^:private get-follow-sets 
+  [grammar first-sets follow-sets non-terminals]
+  (let [new-folsets (process-prods-for-follow
+                       grammar first-sets follow-sets non-terminals)]
+    ; Same as FIRST calculation: keep going until we reach a fixed point
+    ; where the productions don't contribute anything new
+    (if (= follow-sets new-folsets)
+      new-folsets
+      (recur grammar first-sets new-folsets non-terminals))))
+
+(defn ^:private setup-follow-sets [non-terminals]
+  (reduce (fn [map nt] (assoc map nt #{}))
+          {} non-terminals))
+
+(defn calculate-follow-sets 
+  "Calculate the FOLLOW set for a list of productions. Use the same format as
+   described in FRIST. Requires a corrent FIRST."
+  [grammar first-sets]
+  (let [{:keys [all-symbols non-terminals]} (get-grammar-meta grammar)
+        empty-folsets (setup-follow-sets non-terminals)
+        start-symbol (first (first grammar))
+        follow-sets (assoc empty-folsets start-symbol #{:end-of-file})]
+    (get-follow-sets grammar first-sets follow-sets non-terminals)))
